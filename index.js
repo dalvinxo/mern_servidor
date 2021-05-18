f91541e554cf541d2f35e94834ef2832df5d8daa
Dalvin Molina <dalvingamer@gmail.com>
2021-05-18 00:48:58 -0400

    seguro

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const express = require('express');
 const conectarDB = require('./config/db');
 const cors = require('cors');
 
-//password: 6BB5Rnj4BS0PxCzn
-//password_user: T24ZXiqeMOw7H1Xq
-
 // crear el servidor
 const server = express();
 
@@ -19,20 +16,13 @@ server.use(cors());
 server.use(express.json({ extended: true}));
 
 //puerto de la app
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 //importar rutas
 server.use('/api/usuarios', require('./routes/usuarios'));
 server.use('/api/auth', require('./routes/auth'));
 server.use('/api/proyectos', require('./routes/proyectos'));
 server.use('/api/tareas', require('./routes/tareas'));
-
-
-
-// // Definir la página principal
-// server.get('/', (req,res) =>{
-//     res.send('desde la pagina principal')
-// });
 
 //arrancar la app
 server.listen(port,'0.0.0.0', ()=>{
